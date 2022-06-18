e984d447691ed023fec9d32d89aaec9219b9427f
Gabriel Oliveira <gabrieldanuzoliveira358@gmail.com>
2022-06-18 10:54:30 -0300

    Finalizei

--- App.js
+++ App.js
@@ -1,11 +1,61 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
+  const [img, setImg] = useState(require('./src/biscoito.png'));
+  const [textoFrase, setTextoFrase] = useState('');
+
+  let frases = [
+    'A vida trará coisas boas se tiver paciência.',
+    'Demonstre amor e alegria em todas as oportunidades e verá que a paz nasce dentro de si.',
+    'Não compense na ira o que lhe falta na razão.',
+   'Defeitos e virtudes são apenas dois lados da mesma moeda.',
+    'A maior de todas as torres começa no solo.',
+    'Não há que ser forte. Há que ser flexível.',
+    'Todos os dias organiza os seus cabelos, por que não faz o mesmo com o coração?',
+    'Há três coisas que jamais voltam; a flecha lançada, a palavra dita e a oportunidade perdida.',
+    'A juventude não é uma época da vida, é um estado de espírito.',
+    'Podemos escolher o que semear, mas somos obrigados a colher o que plantamos.'
+  ];
+
+  function quebraBiscoito(){
+    let numeroAleatorio = Math.floor(Math.random() * frases.length)
+
+    setTextoFrase(' "' + frases[numeroAleatorio] + '" ');
+    setImg(require('./src/biscoitoAberto.png'))
+  }
+
+  function reiniciar(){
+    setImg(require('./src/biscoito.png'));
+    setTextoFrase('');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      
+      <Image 
+      source={img}
+      style={styles.img}
+      />
+      
+      <Text style={styles.textoFrase}>{textoFrase} </Text>
+
+      <TouchableOpacity style={styles.botao}
+      onPress={quebraBiscoito}
+      >
+        <View style={styles.btnArea}>
+          <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
+
+      <TouchableOpacity 
+      onPress={reiniciar}
+      style={[styles.botao, {marginTop: 15, borderColor: '#121212'}]}>
+        <View style={styles.btnArea}>
+          <Text style={[styles.btnTexto, {color: '#121212'}]}>Reiniciar Biscoito</Text>
+        </View>
+      </TouchableOpacity>
+
     </View>
   );
 }
@@ -13,8 +63,36 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
-});
+  img: {
+    width: 230,
+    height: 230,
+  },
+  textoFrase: {
+    fontSize: 20,
+    color: '#DD7B22',
+    margin: 30,
+    fontStyle: 'italic',
+    textAlign: 'center'
+  },
+  botao: {
+    width: 230,
+    height: 50,
+    borderColor: '#DD7B22',
+    borderWidth: 2,
+    borderRadius: 25
+  },
+  btnArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btnTexto: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: '#DD7B22'
+  }
+})
+
